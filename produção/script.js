a075508ad01c6f170943cc4b4c58673a0842da37
Guizonnn <guilhermejachic@gmail.com>
2026-06-26 10:26:03 -0300

    Add files via upload

---
+++ produção/script.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById("relatorio");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Relatório salvo com sucesso!");
+
+});
